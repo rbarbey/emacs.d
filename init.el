2ffc9965814ee086bdd2f410e6230e39a6259f80
Robert Barbey <robert.barbey@acrolinx.com>
2013-01-28 08:53:03 +0100

    Added shortcut for duplicating lines

--- init.el
+++ init.el
@@ -12,3 +12,17 @@
 (setq auto-mode-alist
       (cons '("\\.md$" . markdown-mode)
 	        auto-mode-alist))
+
+(defun duplicate-line()
+	(interactive)
+	(move-beginning-of-line 1)
+	(kill-line)
+	(yank)
+	(open-line 1)
+	(next-line 1)
+	(yank)
+)
+(global-set-key (kbd "C-d") 'duplicate-line)
+
+(custom-set-variables
+	'(large-file-warning-threshold 100000000))
