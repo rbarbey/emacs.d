975494bfbfa64c1a4fee3f39443c68cb526175ef
Robert Barbey <robert.barbey@acrolinx.com>
2013-04-15 13:11:20 +0200

    Added recentf mode

--- init.el
+++ init.el
@@ -20,6 +20,11 @@
 (add-to-list 'load-path "~/.emacs.d/acrolog-mode")
 (require 'acrolog-mode)
 
+;; open recent files
+(require 'recentf)
+(recentf-mode 1)
+(global-set-key "\C-xf" 'recentf-open-files)
+
 (defun bf-pretty-print-xml-region (begin end)
   "Pretty format XML markup in region. You need to have nxml-mode
 http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
@@ -46,5 +51,20 @@ by using nxml's indentation rules."
   )
 (global-set-key (kbd "C-d") 'duplicate-line)
 
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
 (custom-set-variables
-	'(large-file-warning-threshold 100000000))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(iswitchb-mode t)
+ '(large-file-warning-threshold 100000000))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
